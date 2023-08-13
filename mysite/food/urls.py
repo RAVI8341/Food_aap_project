c1f6da7470d079056b5ca65af4e0fc71a6b26245
RAVI8341 <ravikush444@gmail.com>
2023-08-13 09:48:57 +0530

    site templates implemented

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    #/
     path('', views.index,name='index'),
-    path('item', views.item,name='item'),
+    #/id
+    path('<int:item_id>/',views.detail,name='detail'),
+    
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 # Create your views here.
+#creating a view for all the list of items on Home page
 def index(request):
-    return HttpResponse("hello world")
+    data_item = Item.objects.all()
+    context = {
+        'data_item':data_item,
+    }
+    return render(request,'food/index.html',context)
 
-def item(request):
-    return HttpResponse("this is food app")
+# this view is for details.html for displaying the details about a requested item
+def detail(request, item_id):
+    item=Item.objects.get(pk=item_id)
+    context = {
+        'item':item,
+    }
+    return render(request,'food/detail.html',context)
