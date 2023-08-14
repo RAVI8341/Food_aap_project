660334431aa6b241710009d79cfa35765ddc0e33
RAVI8341 <ravikush444@gmail.com>
2023-08-14 08:33:09 +0530

    Forms are create for the app food

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('', views.index,name='index'),
     #/id
     path('<int:item_id>/',views.detail,name='detail'),
+    # add item
+    path('add',views.create_item, name='create_item'),
+    #update item
+    path('update/<int:id>/', views.update,name='update'), 
+    #delete
+    path('delete/<int:id>/', views.delete,name='delete'),
     
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
 from django.template import loader
+from .forms import ItemForm
 # Create your views here.
 #creating a view for all the list of items on Home page
 def index(request):
@@ -18,3 +19,32 @@ def detail(request, item_id):
         'item':item,
     }
     return render(request,'food/detail.html',context)
+
+#this views is used fpr forms.py where users can update data from the site
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request,'food/item-form.html',{'form':form})
+
+# views for updating data from site
+def update(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item-form.hrml', {'form':form, 'item':item})
+
+# views for deleting item from site
+
+def delete(request, id):
+    item = Item.objects.get(id=id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    return render(request, 'food/item-delete.html', {'item':item})
