e4d1f736f11be49995dddc28f9ccdb10aeb55505
RAVI8341 <ravikush444@gmail.com>
2023-08-23 18:32:30 +0530

    Authientication and user login, logout, profile created

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import redirect,render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 # views for user registration form
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.method)
+        form = RegisterForm(request.POST) 
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'welcome', {username}, 'your account has been created')
-            return redirect('food:index')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'users/register.html',{'form':form})
+
+# creating a users profile view
+@login_required
+def profilepage(request):
+    return render(request, 'users/profile.html',{'user': request.user})
